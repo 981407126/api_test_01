a418a4b06fcf8afa758b86e497cf20c47e5e1b52
SAMSUNG <981407126@qq.com>
2022-06-20 17:47:29 +0800

    自动化测试框架_wshop

---
+++ data/user_manager_data.py
@@ -0,0 +1,10 @@
+
+
+class UserManagerData():
+
+    add_user_data = {
+        "errno":0,
+        "username":"test_csc_02",
+        "password":"123456",
+        "new_user_name":'test_csc_new_02'
+    }
